4b8745988cc3277562457f1adc6527308c3669fa
SaRa-Harahsheh <saraharahsheh0@gmail.com>
2025-02-17 22:02:15 +0300

    Add project files.

---
+++ Task-17-2-2025/Controllers/userController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task_17_2_2025.Controllers
+{
+    public class userController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult productDetails()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Task-17-2-2025/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Task_17_2_2025.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
