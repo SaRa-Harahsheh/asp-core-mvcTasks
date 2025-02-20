23595fc1c34440e4bb9a4034b4d96ff2d24ecf6f
SaRa-Harahsheh <saraharahsheh0@gmail.com>
2025-02-20 05:34:41 +0300

    new folder task 4

--- Task-17-2-2025/Program.cs
+++ Task-17-2-2025/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=admin}/{action=AdminDashboard}/{id?}");
 
 app.Run();
