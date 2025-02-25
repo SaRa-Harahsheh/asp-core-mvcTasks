ce5d60b319b7bf003dd5745557669a25a05e06e7
SaRa-Harahsheh <saraharahsheh0@gmail.com>
2025-02-25 20:08:11 +0300

    tasks

---
+++ start-model-22-02/startModel/startModel/Controllers/DepartmentsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using startModel.Data;
+using startModel.Migrations.AppDb;
+using startModel.Models;
+
+namespace startModel.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public DepartmentsController(AppDbContext appDbContext)
+        {
+             _appDbContext=appDbContext;
+        }
+        public IActionResult DepartmentSection()
+        {
+             IEnumerable< Department > departmentList = _appDbContext.departments.ToList();
+
+            return View(departmentList);
+        }
+    }
+}
